2d2fac88dd22d98a8e4f7a721ea411adaa4f01e4
miller-0509 <caperamiller5@gmail.com>
2026-03-09 13:50:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ load_dotenv()
 app = Flask (__name__)
 
 app.config["SQLALCHEMY DATABASE URI"] = os.getenv("DATABASE_URI", "sqlite:///defo") 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "clave-insegura")
+
+if __name__ == "__main__" :
+    app.run(port=int(os.getenv("PORT", 5000)), debug=os.getenv("FLASK_DEBUG"))
