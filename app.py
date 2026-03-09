54aafaeecfc10da7fb1bb0c8e49a4b0fa78364e1
miller-0509 <caperamiller5@gmail.com>
2026-03-09 15:30:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from models import db
+from flask_migrate import Migrate
 
 load_dotenv()
 
@@ -9,6 +11,10 @@ app = Flask (__name__)
 app.config["SQLALCHEMY DATABASE URI"] = os.getenv("DATABASE_URI", "sqlite:///defo") 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "clave-insegura")
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 if __name__ == "__main__" :
     app.run(port=int(os.getenv("PORT", 5000)), debug=os.getenv("FLASK_DEBUG"))
