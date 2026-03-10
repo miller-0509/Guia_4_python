6d0a2366f5d2329fa5642e9eea7cf67bdea740fa
miller-0509 <caperamiller5@gmail.com>
2026-03-10 15:00:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,3 @@ if __name__ == "__main__" :
     puerto = int(os.getenv("PORT", 5000))
     modo_debug = os.getenv("FLASK_DEBUG") == "True"
     app.run(host='0.0.0.0', port=puerto, debug=modo_debug)
-
-
