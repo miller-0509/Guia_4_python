913980e768ed871c57f632d970ede6da5687c252
miller-0509 <caperamiller5@gmail.com>
2026-03-10 13:56:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from models import db
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-load_dotenv()
+load_dotenv
 
 app = Flask (__name__)
 
-app.config["SQLALCHEMY DATABASE URI"] = os.getenv("DATABASE_URI", "sqlite:///defo") 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", "sqlite:///trapiche_db.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "clave-insegura")
 
