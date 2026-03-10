4f387e840ce195228011e6a8f817fb10d0b683c9
miller-0509 <caperamiller5@gmail.com>
2026-03-10 14:07:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from dotenv import load_dotenv
 from models import db
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
+from  flask_jwt_extended import JWTManager
 
 load_dotenv
 
