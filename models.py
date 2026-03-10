104a79205b21f7708b0c8631e5f493a335772f3d
miller-0509 <caperamiller5@gmail.com>
2026-03-10 14:15:13 -0500

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy ()
 
 class Usuario (db.Model):
-    __tablename = "usuarios"
+    __tablename__ = "usuarios"
+    
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), unique = True, nullable = False)
     
