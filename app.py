945d4f5178bd5557eb4ab8f0f9875c4c5998118d
miller-0509 <caperamiller5@gmail.com>
2026-03-10 13:44:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from dotenv import load_dotenv
 from models import db
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 load_dotenv()
 
@@ -13,8 +14,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "clave-insegura")
 
 db.init_app(app)
-
 migrate = Migrate(app, db)
+jwt = JWTManager(app) 
 
 if __name__ == "__main__" :
     app.run(port=int(os.getenv("PORT", 5000)), debug=os.getenv("FLASK_DEBUG"))
